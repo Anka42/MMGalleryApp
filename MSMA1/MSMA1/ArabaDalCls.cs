e44aa37b8386c2b3336314d7421c4786c2dbd6f7
MEHMET ARICI <arc424267@gmail.com>
2020-11-26 17:16:04 +0300

    Orjinale Yükleme

--- MSMA1/MSMA1/ArabaDalCls.cs
+++ MSMA1/MSMA1/ArabaDalCls.cs
@@ -12,11 +12,11 @@ namespace MSMA1
     class ArabaDalCls
     {
         string linkAdressi;
-        public DataTable Listeleme()
+        private SqlConnection Baglanti()
         {
-            StreamReader streamReader = new StreamReader(@"C:\Users\DEVELOPER\source\repos\OrtakProje\linkAddress.txt");
+            StreamReader streamReader = new StreamReader(@"C:\Users\linkAddress.txt");
             string satir = streamReader.ReadLine();
-            while (satir!=null)
+            while (satir != null)
             {
                 linkAdressi = satir;
                 satir = streamReader.ReadLine();
@@ -26,13 +26,52 @@ namespace MSMA1
             {
                 connection.Open();
             }
-            SqlCommand command = new SqlCommand("Select * from ArabalarMS", connection);
+
+            return connection;
+        }
+
+        public DataTable Listeleme()
+        {
+            SqlConnection connection = Baglanti();
+            SqlCommand command = new SqlCommand("Select * from ArabalarMA", connection);
             SqlDataReader reader = command.ExecuteReader();
             DataTable dataTable = new DataTable();
             dataTable.Load(reader);
             reader.Close();
             connection.Close();
             return dataTable;
+        }
+
+        public void Ekle(ArabaCls arabaCls)
+        {
+            SqlConnection connection = Baglanti();
+            SqlCommand command = new SqlCommand("Insert into ArabalarMA values (@marka,@model,@fiyat,@aciklama)",connection);
+            command.Parameters.AddWithValue("@marka",arabaCls.Marka);
+            command.Parameters.AddWithValue("@model",arabaCls.Model);
+            command.Parameters.AddWithValue("@fiyat", arabaCls.Fiyat);
+            command.Parameters.AddWithValue("@aciklama", arabaCls.Aciklama);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Update(ArabaCls arabaCls)
+        {
+            SqlConnection connection = Baglanti();
+            SqlCommand command = new SqlCommand("Update ArabalarMA set Marka=@marka,Model=@model,Fiyat=@fiyat,Aciklama=@aciklama where Id=@id",connection);
+            command.Parameters.AddWithValue("@marka", arabaCls.Marka);
+            command.Parameters.AddWithValue("@model", arabaCls.Model);
+            command.Parameters.AddWithValue("@fiyat", arabaCls.Fiyat);
+            command.Parameters.AddWithValue("@aciklama", arabaCls.Aciklama);
+            command.Parameters.AddWithValue("@id",arabaCls.Id);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Delete(int id)
+        {
+            SqlConnection connection = Baglanti();
+            SqlCommand command = new SqlCommand("Delete from ArabalarMA where Id=@id",connection);
+            command.Parameters.AddWithValue("@id",id);
+            command.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
